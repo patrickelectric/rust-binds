ce22c0c74851a0f344b328f4a2f6456d8790975a
Patrick José Pereira <patrickelectric@gmail.com>
2023-04-14 14:28:50 -0300

    lib: Add nested and more complex elements
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,56 @@ use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 use rand::Rng;
 
+#[derive(Clone, Debug)]
 #[repr(C)]
 #[pyclass]
-pub struct Point {
+pub enum Material {
+    Plastic,
+    Rubber,
+}
+
+#[derive(Clone, Debug)]
+#[repr(C)]
+#[pyclass]
+pub struct Size2D {
     #[pyo3(get)]
-    pub x: f64,
+    pub width: f64,
     #[pyo3(get)]
-    pub y: f64,
+    pub height: f64,
+}
+
+#[derive(Clone, Debug)]
+#[repr(C)]
+#[pyclass]
+pub struct Tire {
+    #[pyo3(get)]
+    pub material: Material,
+    #[pyo3(get)]
+    pub pressure: f64,
+    #[pyo3(get)]
+    pub size: Size2D,
 }
 
 #[pyfunction]
 #[no_mangle]
-pub extern "C" fn create_random_point() -> Point {
+pub extern "C" fn create_random_tire() -> Tire {
     let mut rng = rand::thread_rng();
-    Point {
-        x: rng.gen_range(-100.0..100.0),
-        y: rng.gen_range(-100.0..100.0),
+    Tire {
+        material: Material::Plastic,
+        pressure: rng.gen_range(30.0..60.0),
+        size: Size2D {
+            width: rng.gen_range(5.0..10.0),
+            height: rng.gen_range(10.0..20.0),
+        },
     }
 }
 
 #[pymodule]
 fn rust_bindings(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<Point>()?;
-    m.add_wrapped(wrap_pyfunction!(create_random_point))?;
+    m.add_class::<Tire>()?;
+    m.add_class::<Size2D>()?;
+    m.add_class::<Material>()?;
+    m.add_wrapped(wrap_pyfunction!(create_random_tire))?;
 
     Ok(())
 }
