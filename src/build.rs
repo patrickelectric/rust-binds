bdb89c21171b7ca6a8d2b1c234c8fbd5b16dfa95
Patrick José Pereira <patrickelectric@gmail.com>
2023-04-14 13:45:21 -0300

    build: Generate header file into target directory
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/build.rs
+++ src/build.rs
@@ -2,7 +2,12 @@ extern crate cbindgen;
 
 fn main() {
     let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let target_dir = std::env::var("CARGO_TARGET_DIR").unwrap_or_else(|_| "target".to_string());
+    let profile = std::env::var("PROFILE").unwrap_or_else(|_| "debug".to_string());
+    let bindings_file = std::path::Path::new(&target_dir)
+        .join(&profile)
+        .join("bindings.h");
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
-        .write_to_file("bindings.h");
+        .write_to_file(bindings_file);
 }
